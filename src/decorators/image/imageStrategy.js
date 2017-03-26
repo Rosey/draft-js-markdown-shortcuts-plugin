9f754095b8a40b76a3bef91d82a89f6c6b14ab41
Michael Lefebvre <michael@scenedata.com>
2017-03-26 09:05:19 +0200

    Dirty fix for conflict with mention plugin
    ngs/draft-js-markdown-shortcuts-plugin/issues/15

--- src/decorators/image/imageStrategy.js
+++ src/decorators/image/imageStrategy.js
@@ -1,11 +1,13 @@
 const createImageStrategy = () => {
   const findImageEntities = (contentBlock, callback, contentState) => {
     contentBlock.findEntityRanges((character) => {
-      const entityKey = character.getEntity();
-      return (
-        entityKey !== null &&
-        contentState.getEntity(entityKey).getType() === 'IMG'
-      );
+      try {
+        const entityKey = character.getEntity();
+        return (
+          entityKey !== null &&
+          contentState.getEntity(entityKey).getType() === 'IMG'
+        );
+      } catch( e ) {}
     }, callback);
   };
   return findImageEntities;
--- src/decorators/link/linkStrategy.js
+++ src/decorators/link/linkStrategy.js
@@ -1,11 +1,13 @@
 const createLinkStrategy = () => {
   const findLinkEntities = (contentBlock, callback, contentState) => {
     contentBlock.findEntityRanges((character) => {
-      const entityKey = character.getEntity();
-      return (
-        entityKey !== null &&
-        contentState.getEntity(entityKey).getType() === 'LINK'
-      );
+      try {
+        const entityKey = character.getEntity();
+        return (
+          entityKey !== null &&
+          contentState.getEntity(entityKey).getType() === 'LINK'
+        );
+      } catch( e ) {}
     }, callback);
   };
   return findLinkEntities;
